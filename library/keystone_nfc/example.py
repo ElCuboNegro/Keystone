3f8899483cc5d0dd794fa23837ade0e928d8df40
Juan Jose Albán Ortiz <jalban.arq@gmail.com>
2026-03-12 04:03:40 -0500

    Generalize discovery tools for cookiecutter template

--- library/keystone_nfc/example.py
+++
@@ -1,6 +0,0 @@
-# This file has been moved to DEMO/nfc_reader_demo.py
-# Run: python DEMO/nfc_reader_demo.py
-raise ImportError(
-    "keystone_nfc.example has moved to DEMO/nfc_reader_demo.py\n"
-    "Run: python DEMO/nfc_reader_demo.py"
-)
